9d7802ccc999868af1af664142fcd71b3a9a352c
pakornkiat <pakornkiat.jims@g.swu.ac.th>
2026-03-12 13:54:39 +0700

    lab week7

---
+++ webdemo/models/expense.js
@@ -0,0 +1,66 @@
+class Expense {
+  /**
+   Represents a Expense.
+   @constructor
+   @param {date} date - The date of the expense.
+   @param {float} income - The author of the book.
+   @param {float} expense
+   @param {string} detail
+ */
+  constructor(date, income, expense, detail) {
+    this.date = date;
+    this.income = parseFloat(income) || 0;
+    this.expense = parseFloat(expense) || 0;
+    this.detail = detail || '';
+  }
+}
+
+/**
+ * Class ExpenseModel.
+ * @class
+ */
+class ExpenseModel {
+  constructor() {
+    this.expenses = [];
+  }
+
+/**
+ * add expense numbers passed to the function.
+ * @param {float} expense - A positive number.
+ */
+  add(expense) {
+    this.expenses.push(expense);
+  }
+
+/**
+ * return expense numbers from stack.
+ * @return {array} expense - array of positive number
+ */
+
+  getAll() {
+    return this.expenses;
+  }
+
+/**
+ * @return {number}
+ */
+  getTotalIncome() {
+    return this.expenses.reduce((sum, exp) => sum + exp.income, 0);
+  }
+
+/**
+ * @return {number}
+ */
+  getTotalExpense() {
+    return this.expenses.reduce((sum, exp) => sum + exp.expense, 0);
+  }
+
+/**
+ * @return {number}
+ */
+  getMoneyLeft() {
+    return this.getTotalIncome() - this.getTotalExpense();
+  }
+}
+
+module.exports = { Expense, ExpenseModel };
